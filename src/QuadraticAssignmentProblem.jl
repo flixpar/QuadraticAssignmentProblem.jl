3c76cef8b32a4be7ea673362673d369f2b2f36a6
Felix Parker <flixpar@gmail.com>
2021-05-07 10:16:19 -0400

    changed package exports

--- src/QuadraticAssignmentProblem.jl
+++ src/QuadraticAssignmentProblem.jl
@@ -1,15 +1,24 @@
 module QuadraticAssignmentProblem
 
 include("solvers/ip.jl")
+include("solvers/lp.jl")
 include("solvers/faq.jl")
 include("solvers/mms.jl")
+include("solvers/random.jl")
 
 include("util/generators.jl")
 include("util/graphmatching.jl")
 include("util/metrics.jl")
 
-export qap_ip
-export faq, sgm
+export qap_ip, lap_ip
+export lap_relaxation
+export faq
 export mms
+export qap_random
+
+export uniform_matrix
+export metric_matrix
+
+export qap_objective
 
 end
