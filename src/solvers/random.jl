45e1fde399e0cd167f4e662d6c4f34a0f65dd890
Felix Parker <flixpar@gmail.com>
2021-05-10 10:25:43 -0400

    overhaul random search method

--- src/solvers/random.jl
+++ src/solvers/random.jl
@@ -4,24 +4,25 @@ using Random
 """
 Approximately solve the QAP through random sampling.
 """
-function qap_random(A, B; iter=100)
+function qap_random(A, B, obj; iter=100)
+	@assert obj in [:min, :max]
 	N = size(A, 1)
-	Im = Matrix(I, N, N)
 
-	score = Inf
-	P = nothing
-	matching = nothing
+	best_sol = ((obj == :min) ? Inf : -Inf, nothing)
 
 	for i in 1:iter
 		perm = randperm(N)
-		Q = Im[perm,:]
-		s = norm(A*Q - Q*B, 2)^2
-		if s < score
-			score = s
-			P = Q
-			matching = perm
+		s = sum(A .* B[perm, perm])
+
+		if obj == :min && s < best_sol[1]
+			best_sol = (s, perm)
+		elseif obj == :max && s > best_sol[1]
+			best_sol = (s, perm)
 		end
 	end
 
-	return Int.(P), matching
+	perm = best_sol[2]
+	P = Matrix(I, N, N)[perm,:]
+
+	return P, perm
 end
