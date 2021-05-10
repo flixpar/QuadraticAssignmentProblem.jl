8cd72188273237a23de4b37f6a222feef72e7806
Felix Parker <flixpar@gmail.com>
2021-05-10 10:57:00 -0400

    overhaul match ratio

--- src/util/metrics.jl
+++ src/util/metrics.jl
@@ -13,38 +13,12 @@ function qap_objective(A::AbstractArray{<:Real,2}, B::AbstractArray{<:Real,2}, p
 end
 
 """
-Compute the accuracy of an estimated graph matching relative to the canonical
-matching, excluding seeeds.
+Compute the match ratio of two permutations.
 """
-function match_ratio(true_match::Array{Int,2}, est_match::Array{Int,2}; seeds::Int=0)
-
-	true_match = sortslices(true_match, dims=1)
-	est_match  = sortslices(est_match, dims=1)
-	@assert true_match[:,1] == est_match[:,1]
-
-	n = size(true_match, 1)
-	acc = sum(true_match[seeds+1:end,2] .== est_match[seeds+1:end,2]) / (n - seeds)
-	return acc
-
-end
-
-"""
-Compute the accuracy of an estimated graph matching relative to the canonical
-graph matching, excluding seeds and errors.
-"""
-function match_ratio(true_match::Array{Int,2}, est_match::Array{Int,2}, seeds::Int, errors::Array{Int,2})
-
-	ignore = union(1:seeds, errors[:,1], errors[:,2])
-	keepind = setdiff(1:size(true_match, 1), ignore)
-
-	true_match = sortslices(true_match, dims=1)
-	est_match  = sortslices(est_match, dims=1)
-	@assert true_match[:,1] == est_match[:,1]
-
-	true_match = true_match[keepind,2]
-	est_match  = est_match[keepind,2]
-
-	acc = sum(true_match .== est_match) / length(true_match)
-	return acc
-
+function match_ratio(perm1::Array{Int,1}, perm2::Array{Int,1})
+	@assert length(perm1) == length(perm2)
+	@assert isperm(perm1) && isperm(perm2)
+	N = length(perm1)
+	mr = sum(perm1 .== perm2) / N
+	return mr
 end
