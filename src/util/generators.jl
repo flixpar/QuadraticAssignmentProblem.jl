00ed4f287cdce784ba1e225c38176213c887d1cd
Felix Parker <flixpar@gmail.com>
2021-05-10 10:53:29 -0400

    add descriptions of matrix generators

--- src/util/generators.jl
+++ src/util/generators.jl
@@ -2,16 +2,25 @@ using Random
 using LinearAlgebra
 
 
+"""
+Generate an N×N matrix with uniformly random entries in [0,1].
+"""
 function uniform_matrix(N)
 	return rand(N, N)
 end
 
+"""
+Generate a random N×N matrix with entries satisfying the triangle inequality.
+"""
 function metric_matrix(N; dim=5, p=2)
 	points = rand(N, dim)
 	M = [norm(points[i,:] - points[j,:], p) for i in 1:N, j in 1:N]
 	return M
 end
 
+"""
+Generate a random 0-1 N×N matrix with edge probability p.
+"""
 function zeroone_matrix(N; p=0.25, selfedges=true, symmetric=false)
 	M = rand(N, N) .≤ p
 	if symmetric
